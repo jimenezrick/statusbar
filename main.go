55034a352ef11464570052be0c434340fe33a2b9
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-12-24 17:42:39 +0100

    .

--- main.go
+++ main.go
@@ -4,65 +4,7 @@ package main
 // XXX /proc/net/dev
 // XXX /proc/diskstats
 
-// #cgo CFLAGS: -Wall
-// #cgo LDFLAGS: -lxcb
-// #include <stdlib.h>
-// #include "wm_name.h"
-import "C"
-
-import (
-	"unsafe"
-	"strings"
-	"time"
-)
-
 func main() {
-	xconn := C.connect_x()
-	if xconn == nil {
-		panic("can't connect to X server")
-	}
-	defer C.disconnect_x(xconn)
-
-	if C.set_screen(xconn) == -1 {
-		panic("can't find screen")
-	}
-
-	show_animation(xconn, ">", 20, 25, 2)
-	set_wm_name(xconn, "")
+	go updater()
+	updateStats()
 }
-
-func set_wm_name(xconn *C.xconn_t, name string) {
-	str := C.CString(name)
-	defer C.free(unsafe.Pointer(str));
-	if C.set_wm_name(xconn, str) == -1 {
-		panic("can't set window manager name")
-	}
-}
-
-func show_animation(xconn *C.xconn_t, pattern string, len int, pause int, times int) {
-	for t := 0; t < times; t++ {
-		for l := len; l > 0; l-- {
-			set_wm_name(xconn, strings.Repeat(pattern, l))
-			time.Sleep(time.Duration(pause) * time.Millisecond)
-		}
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ stats.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func updateStats() {
+	for {
+		s := formatStats(time.Now(), 0.025, 2.5*1024, 0.2, 20, 300000000) // XXX
+		select {
+		case statsUpdates <- s:
+		default:
+			println("--- SKIP ---") // TODO: Remove
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func formatStats(t time.Time, load float32, mem int, io float32, up, down int) string {
+	return fmt.Sprintf("%s [%.2fL] [%s] [%.2fIO] [%s/%s]",
+		t.Format(time.ANSIC), load, addUnits(mem), io, addUnits(up), addUnits(down))
+}
+
+func addUnits(x int) string {
+	switch {
+	case x >= 1024*1024*1024:
+		return fmt.Sprintf("%.2fG", float32(x)/(1024*1024*1024))
+	case x >= 1024*1024:
+		return fmt.Sprintf("%.2fM", float32(x)/(1024*1024))
+	case x >= 1024:
+		return fmt.Sprintf("%.2fK", float32(x)/1024)
+	}
+	return fmt.Sprintf("%.2f", float32(x))
+}

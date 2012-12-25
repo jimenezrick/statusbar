13ab8d268866558c02678cc7c6992f27d985e508
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-12-25 02:23:09 +0100

    Recover from panic

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "flag"
+import (
+	"os"
+	"fmt"
+	"flag"
+)
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], r)
+		}
+	}()
+
 	flag.StringVar(&disk, "d", "sda", "disk device")
 	flag.StringVar(&iface, "i", "eth0", "net interface")
 	flag.IntVar(&pause, "p", 1, "update interval (seconds)")
